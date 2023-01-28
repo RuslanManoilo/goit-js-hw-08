e08b9cddaa1de52b05e84257f39f2468dfe0a2dc
Ruslan <manoiloruslan@gmail.com>
2023-01-28 23:43:59 +0200

    Task 2 - start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
 
+console.log(Player);
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+
+player.on('timeupdate', onPlay);
+
+function onPlay(event) {
+    console.log(event.seconds);
+
+    timeArrey.push(event.seconds);
+};
+
+const timeArrey = [];
+console.log(timeArrey);
+
+
+
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
