3d7e093670131631d688ccbe2becd05a0720a8b7
Ruslan <manoiloruslan@gmail.com>
2023-02-01 21:04:38 +0200

    Task 03 - complete!!!

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,10 @@ var throttle = require('lodash.throttle');
 
 
 const formElem = document.querySelector('.feedback-form');
+// console.log(formElem);
 
 formElem.addEventListener('input', throttle(onFormInput, 500));
 formElem.addEventListener('submit', formSubmit);
-
-const objForm = {
-    email: emailValue,
-    message: messageValue,
-}
-// console.log(objForm);
 
 getValueForm();
 
@@ -20,24 +15,28 @@ function onFormInput(event) {
     // console.log(emailValue);
     // console.log(messageValue);
 
+    const objForm = {
+        email: emailValue,
+        message: messageValue,
+    };
+    // console.log(objForm);
+
     localStorage.setItem("feedback-form-state", JSON.stringify(objForm));
 };
 
 function formSubmit(event) {
     event.preventDefault();
-    
-    localStorage.removeItem("feedback-form-state");
+
+    const objForm = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(objForm);
 
     event.currentTarget.reset();
-
-    console.log(objForm);   
-};
+}
 
 function getValueForm() {
-   const getObjForm = JSON.parse(localStorage.getItem("feedback-form-state"));
-
-    if (getObjForm) {
-        document.querySelector('input') = getObjForm.email;
-        document.querySelector('textarea') = getObjForm.message;
-    }
+    const saveData = JSON.parse(localStorage.getItem("feedback-form-state"));
+        if (saveData) {
+            formElem.elements.email.value = saveData.email;
+            formElem.elements.message.value = saveData.message;
+        };
 };
